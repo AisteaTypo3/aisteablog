c960001faa35d3efd1413488dc137ccee565be40
Yannick Aister <yannick.aister@aistee.de>
2026-03-24 20:27:49 +0100

    Add Seo Graph

--- Classes/Controller/PostController.php
+++ Classes/Controller/PostController.php
@@ -9,6 +9,7 @@ use Aistea\Aisteablog\Domain\Model\Post;
 use Aistea\Aisteablog\Domain\Repository\CategoryRepository;
 use Aistea\Aisteablog\Domain\Repository\PostRepository;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\MetaTag\MetaTagManagerRegistry;
 use TYPO3\CMS\Core\Pagination\SimplePagination;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Pagination\QueryResultPaginator;
@@ -18,6 +19,7 @@ class PostController extends ActionController
     public function __construct(
         private readonly PostRepository $postRepository,
         private readonly CategoryRepository $categoryRepository,
+        private readonly MetaTagManagerRegistry $metaTagManagerRegistry,
     ) {}
 
     public function listAction(int $currentPage = 1): ResponseInterface
@@ -41,6 +43,8 @@ class PostController extends ActionController
             ->reset()
             ->setCreateAbsoluteUri(true)
             ->uriFor('show', ['post' => $post]);
+
+        $this->setOpenGraphTags($post, $shareUrl);
 
         $this->view->assignMultiple([
             'post'       => $post,
@@ -67,5 +71,41 @@ class PostController extends ActionController
         ]);
 
         return $this->htmlResponse();
+    }
+
+    private function setOpenGraphTags(Post $post, string $shareUrl): void
+    {
+        $tags = [
+            'og:type'             => 'article',
+            'og:title'            => $post->getTitle(),
+            'og:description'      => $post->getTeaser(),
+            'og:url'              => $shareUrl,
+            'twitter:card'        => 'summary_large_image',
+            'twitter:title'       => $post->getTitle(),
+            'twitter:description' => $post->getTeaser(),
+        ];
+
+        $coverImage = $post->getFirstCoverImage();
+        if ($coverImage !== null) {
+            $imageUrl = $coverImage->getOriginalResource()->getPublicUrl();
+            if ($imageUrl !== null && $imageUrl !== '') {
+                if (!str_starts_with($imageUrl, 'http')) {
+                    $normalizedParams = $this->request->getAttribute('normalizedParams');
+                    $imageUrl = rtrim($normalizedParams->getSiteUrl(), '/') . '/' . ltrim($imageUrl, '/');
+                }
+                $tags['og:image']     = $imageUrl;
+                $tags['og:image:alt'] = $post->getTitle();
+                $tags['twitter:image'] = $imageUrl;
+            }
+        }
+
+        foreach ($tags as $property => $value) {
+            if ($value === '') {
+                continue;
+            }
+            $this->metaTagManagerRegistry
+                ->getManagerForProperty($property)
+                ->addProperty($property, $value);
+        }
     }
 }
